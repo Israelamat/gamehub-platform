7a5bc04af7472b889bfcbe8dc3246291a051c4ec
Israelamat <israelamatgil@gmail.com>
2026-04-25 15:36:37 +0200

    Redesign footer and fix scroll reveal animation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ScrollRevealDirective } from '../directives/scroll-reveal';
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.css'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [ScrollRevealDirective],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   email: string = "gamehub.info@gmail.com";
