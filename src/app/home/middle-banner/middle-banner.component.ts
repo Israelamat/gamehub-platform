ef230c9e83604fb246760055a8314b1cd7e1ed92
Israelamat <israelamatgil@gmail.com>
2026-04-25 12:13:24 +0200

    Implement ScrollRevealDirective for on-scroll animations in Middle Bar component

--- src/app/home/middle-banner/middle-banner.component.ts
+++ src/app/home/middle-banner/middle-banner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal';
 
 @Component({
     selector: 'app-middle-banner',
     standalone: true,
-    imports: [],
+    imports: [ScrollRevealDirective],
     templateUrl: './middle-banner.component.html',
     styleUrl: './middle-banner.component.css'
 })
