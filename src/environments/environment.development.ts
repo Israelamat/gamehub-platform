c450d85fbc758cb36f9f74f89734b5b1b1098a6f
Israelamat <israelamatgil@gmail.com>
2026-05-02 01:49:36 +0200

    remove environments from source control

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.development.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000',
-  STORAGE_KEY: "sk_9fA7xQ2LmP0vZr8TnW4cYhK6uJ3dB1sE",
-  Mail: "arincaster127@gmail.com"
-};
