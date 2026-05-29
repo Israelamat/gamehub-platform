6322ce3b22f3f748dafc31411fc6b65107dbf0ca
Israelamat <israelamatgil@gmail.com>
2026-05-29 02:11:45 +0200

    update platform feature cards with unique titles

--- src/app/home/roadmap/roadmap.component.ts
+++ src/app/home/roadmap/roadmap.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ScrollRevealDirective } from '../../directives/scroll-reveal';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-roadmap',
     standalone: true,
-    imports: [ScrollRevealDirective],
+    imports: [ScrollRevealDirective, RouterModule],
     templateUrl: './roadmap.component.html',
     styleUrl: './roadmap.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
